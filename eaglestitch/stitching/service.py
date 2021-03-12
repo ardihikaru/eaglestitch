e886136ba597155054319bae95f9bb5c83c17506
Muhammad Febrian Ardiansyah <ardihikaru3@gmail.com>
2021-03-13 02:33:25 +0800

    - code refactor - get stored input image paths

--- eaglestitch/stitching/service.py
+++ eaglestitch/stitching/service.py
@@ -35,22 +35,11 @@ class StitchingService(asab.Service):
 		L.warning("[STICHING] PERFORMING STITCHING FOR THIS TUPLE OF IMAGES")
 
 		# perform stitching
-		stitch_status, stitch_result = self._stitch_imgs(collected_imgs, batch_num)
+		stitch_status, stitch_result, input_imgs = self._stitch_imgs(collected_imgs, batch_num)
 		if not stitch_status:
 			return False
 		L.warning("[STICHING] STITCHING SUCCEED!")
 		# L.log(LOG_NOTICE, "[STICHING] STITCHING SUCCEED!")
-
-		# if stitching succeed: [1] store stitched images into output directory
-		if stitch_status:
-			self._save_stitched_imgs_to_disk(stitch_result)
-		L.warning("[STICHING] STITCHING RESULT HAS BEEN STORED!")
-		# L.log(LOG_NOTICE, "[STICHING] STITCHING RESULT HAS BEEN STORED!")
-
-		# if stitching succeed: [2] store input images into output directory
-		input_imgs = self._save_input_imgs_to_disk(collected_imgs)
-		L.warning("[STICHING] INPUT IMAGES HAS BEEN STORED!")
-		# L.log(LOG_NOTICE, "[STICHING] INPUT IMAGES HAS BEEN STORED!")
 
 		# if storing images succeed, save all those information into database
 		data_message_json = {
@@ -76,19 +65,7 @@ class StitchingService(asab.Service):
 			sticher = Stitch(imgs, batch_num, self.config)
 			sticher.run()
 
-			return True, sticher.get_stitch_result()
+			return True, sticher.get_stitch_result(), sticher.get_stored_input_imgs()
 		except Exception as e:
 			L.error("Stitching failed due to: `{}`".format(e))
 			return False, None
-
-	def _save_stitched_imgs_to_disk(self, stitch_result):
-		self.storage_svc.save_img_to_disk(stitch_result)
-
-	def _save_input_imgs_to_disk(self, collected_imgs):
-		stored_imgs = []
-		for img in collected_imgs:
-			stored_img = self.storage_svc.save_img_to_disk(img)
-			if stored_img is not None:
-				stored_imgs.append(stored_img)
-
-		return stored_imgs
