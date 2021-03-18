dc64d21ea95a04bfe488194a52cc2327f879bcab
Muhammad Febrian Ardiansyah <ardihikaru3@gmail.com>
2021-03-18 17:07:27 +0800

    add different input files

--- tests/net_publisher_img_camera_noskip.py
+++ tests/net_publisher_img_camera_noskip.py
@@ -84,6 +84,13 @@ encoder_format = [
 window_title = "output-raw"
 # cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture("/home/ardi/devel/nctu/IBM-Lab/eaglestitch/data/videos/0312_2_CUT.mp4")
+# cap = cv2.VideoCapture("/home/ardi/devel/nctu/IBM-Lab/eaglestitch/data/videos/long_gun/0316_LongGun_1_5_LtoR.mp4")
+# cap = cv2.VideoCapture("/home/ardi/devel/nctu/IBM-Lab/eaglestitch/data/videos/long_gun/0316_LongGun_1_5_RtoL.mp4")
+# cap = cv2.VideoCapture("/home/ardi/devel/nctu/IBM-Lab/eaglestitch/data/videos/long_gun/0316_LongGun_1_9_LtoR.mp4")
+# cap = cv2.VideoCapture("/home/ardi/devel/nctu/IBM-Lab/eaglestitch/data/videos/long_gun/0316_LongGun_1_9_RtoL.mp4")
+# cap = cv2.VideoCapture("/home/ardi/devel/nctu/IBM-Lab/eaglestitch/data/videos/long_gun/0316_LongGun_2_5_LtoR.mp4")
+# cap = cv2.VideoCapture("/home/ardi/devel/nctu/IBM-Lab/eaglestitch/data/videos/long_gun/0316_LongGun_3_5_LtoR.mp4")
+# cap = cv2.VideoCapture("/home/ardi/devel/nctu/IBM-Lab/eaglestitch/data/videos/samer/0312_1_LtoR_1.mp4")
 cv2.namedWindow(window_title, cv2.WND_PROP_FULLSCREEN)
 # cv2.resizeWindow("Image", 1920, 1080)  # Enter your size
 cv2.resizeWindow(window_title, 800, 550)  # Enter your size
@@ -92,6 +99,7 @@ while cap.isOpened():
 	_frame_id += 1
 	# ret = a boolean return value from getting the frame, frame = the current frame being projected in the video
 	try:
+		print(" ### _frame_id:", _frame_id)
 		ret, frame = cap.read()
 
 		# resize the frame; Default VGA (640 x 480)
