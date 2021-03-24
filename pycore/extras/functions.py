67a55f844f6c71cdb225aa321deedbc01b23be0b
Muhammad Febrian Ardiansyah <ardihikaru3@gmail.com>
2021-03-24 11:57:20 +0800

    add common functions related with zenoh pubsub

--- pycore/extras/functions.py
+++ pycore/extras/functions.py
@@ -27,3 +27,25 @@ def is_img_exist(target_path):
 		return True
 	else:
 		return False
+
+
+def decrypt_str(int_val, byteorder="little"):
+	decrypted_bytes = int_val.to_bytes((int_val.bit_length() + 7) // 8, byteorder)  # byteorder must be either 'little' or 'big'
+	decrypted_str = decrypted_bytes.decode('utf-8')
+	return decrypted_str
+
+
+def extract_drone_id(data, img_len):
+	drone_idx = img_len
+	return decrypt_str(int(data[drone_idx][0]))
+
+
+def extract_t0(data, img_len):
+	to_p1_idx = img_len + 2
+	to_p2_idx = img_len + 3
+
+	t0 = "{}.{}".format(
+		data[to_p1_idx][0],
+		data[to_p2_idx][0],
+	)
+	return float(t0)
