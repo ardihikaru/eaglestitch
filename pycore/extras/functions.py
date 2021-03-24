70999eba3c0e97bba1edcf9fc352c7587278a84e
Muhammad Febrian Ardiansyah <ardihikaru3@gmail.com>
2021-03-24 11:59:39 +0800

    add `encrypt_str` function and add function descriptions

--- pycore/extras/functions.py
+++ pycore/extras/functions.py
@@ -29,18 +29,28 @@ def is_img_exist(target_path):
 		return False
 
 
+def encrypt_str(str_val, byteorder="little"):
+	""" Converts a string value into an integer value """
+	encrypted_bytes = str_val.encode('utf-8')
+	encrypted_val = int.from_bytes(encrypted_bytes, byteorder)  # `byteorder` must be either 'little' or 'big'
+	return encrypted_val  # max 19 digit
+
+
 def decrypt_str(int_val, byteorder="little"):
+	""" Converts an integer value into a string value """
 	decrypted_bytes = int_val.to_bytes((int_val.bit_length() + 7) // 8, byteorder)  # byteorder must be either 'little' or 'big'
 	decrypted_str = decrypted_bytes.decode('utf-8')
 	return decrypted_str
 
 
 def extract_drone_id(data, img_len):
+	""" Extract drone_id captured by Zenoh's Consumer """
 	drone_idx = img_len
 	return decrypt_str(int(data[drone_idx][0]))
 
 
 def extract_t0(data, img_len):
+	""" Extract timestamp (t0) captured by Zenoh's Consumer """
 	to_p1_idx = img_len + 2
 	to_p2_idx = img_len + 3
 
